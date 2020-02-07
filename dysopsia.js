2748e9796c914b8ae519cff6d6b49c4b2cbb54ad
frontend-developer <tegscramp@gmail.com>
2020-02-07 18:40:13 +0200

    Update dysopsia.js

--- dysopsia.js
+++ dysopsia.js
@@ -1,7 +1,7 @@
+let style_elem = document.createElement('style');
+style_elem.innerHTML = 'body.dysopsia{filter:grayscale(100%);background-color:#fff;}';
+document.querySelector('body').append(style_elem);
  var dysopsia = document.getElementById('dysopsia_button');
  dysopsia.addEventListener('click', function () {
-  let style_elem = document.createElement('style');
-  style_elem.innerHTML = 'body.dysopsia{filter:grayscale(100%);background-color:#fff;}';
-  document.querySelector('body').append(style_elem);
-  document.querySelector('body').classList.toggle('dysopsia');
- }, false);
+ document.querySelector('body').classList.toggle('dysopsia');
+}, false);
